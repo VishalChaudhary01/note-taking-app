9262976fbc2e10a83ae5fed2fefe7aeb2adddae6
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2025-02-04 13:45:22 +0530

    ecntralize error handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express, { Request, Response } from 'express';
 import { connectDB } from './config/db';
+import { errorHandler } from './middlewares/error.handler';
+import { CustomError } from './utils/custom.error';
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 connectDB();
@@ -10,7 +14,12 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Healthy Server');
 })
 
-const PORT = process.env.PORT || 3000;
+app.all('*', () => {
+    throw new CustomError('API url not found', 'NOT_FOUND');
+});
+
+app.use(errorHandler);
+
 app.listen(3000, () => {
     console.log(`Server is started on http://localhost:${PORT}`);
 })

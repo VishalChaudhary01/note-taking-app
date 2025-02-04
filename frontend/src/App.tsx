ef8f3df004dfd45f2e810895060c7f0c36f176f9
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2025-02-04 12:25:55 +0530

    initial frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold">Hi there</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
